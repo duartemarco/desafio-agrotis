98cfa17d46132f1fb6cc1a0c92bebc6fd5810cb8
Marco Túlio Duarte <marcoduarte96sm@gmail.com>
2025-04-10 10:10:04 -0300

    <agrotis> corrigindo pessoaMapper para usar padrão do lombok, evitar nullpointerexception

--- src/main/java/com/agrotis/agrotis_backend/application/dto/PessoaDTO.java
+++ src/main/java/com/agrotis/agrotis_backend/application/dto/PessoaDTO.java
@@ -17,22 +17,8 @@ public class PessoaDTO {
     private String nome;
     private LocalDateTime dataInicial;
     private LocalDateTime dataFinal;
-
     private PropriedadeDTO infosPropriedade;
-
-    public PessoaDTO(Long id, String nome, LocalDateTime dataInicial, LocalDateTime dataFinal, PropriedadeDTO infosPropriedade, LaboratorioDTO laboratorio, String observacoes) {
-        this.id = id;
-        this.nome = nome;
-        this.dataInicial = dataInicial;
-        this.dataFinal = dataFinal;
-        this.infosPropriedade = infosPropriedade;
-        this.laboratorio = laboratorio;
-        this.observacoes = observacoes;
-    }
-
     private LaboratorioDTO laboratorio;
-
     private String observacoes;
-
 
 }
